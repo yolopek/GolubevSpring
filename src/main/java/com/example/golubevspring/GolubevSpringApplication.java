7ae38918e78fb7937c05c2d9a295902be6cc4d1a
igolubev <yolo.pek1@gmail.com>
2023-09-19 18:24:47 +0500

    First commit

---
+++ src/main/java/com/example/golubevspring/GolubevSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.golubevspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolubevSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolubevSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/golubevspring/GolubevSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.golubevspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolubevSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
